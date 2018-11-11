78541d416a39981912ed74cb863279abc96130f3
Sieg <41359981+do-sieg@users.noreply.github.com>
2018-11-11 17:04:43 +0300

    Create Jump Fix.rb

---
+++ JumpFix/Jump Fix.rb
@@ -0,0 +1,32 @@
+#==============================================================================
+# ** Jump Fix
+#------------------------------------------------------------------------------
+#  By Siegfried (http://saleth.fr)
+#------------------------------------------------------------------------------
+#  This script fixes the jump movement for characters in RPG Maker VX Ace,
+# adding a check for passability before jumping outside of the map.
+#==============================================================================
+
+class Game_Character < Game_CharacterBase
+  #--------------------------------------------------------------------------
+  # * Jump
+  #     x_plus : x-coordinate plus value
+  #     y_plus : y-coordinate plus value
+  #--------------------------------------------------------------------------
+  def jump(x_plus, y_plus)
+    if x_plus.abs > y_plus.abs
+      set_direction(x_plus < 0 ? 4 : 6) if x_plus != 0
+    else
+      set_direction(y_plus < 0 ? 8 : 2) if y_plus != 0
+    end
+    if $game_map.passable?(@x + x_plus, @y + y_plus, 0)
+      @x += x_plus
+      @y += y_plus
+      distance = Math.sqrt(x_plus * x_plus + y_plus * y_plus).round
+      @jump_peak = 10 + distance - @move_speed
+      @jump_count = @jump_peak * 2
+      @stop_count = 0
+      straighten
+    end
+  end
+end
